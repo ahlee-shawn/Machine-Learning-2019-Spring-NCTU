e99cf9d7410f73834c140c6f8f0b0301430315cf
leeang6969 <leeang6969.cs05@g2.nctu.edu.tw>
2019-02-28 12:07:42 +0800

    Update hw1.py

--- Lab1/hw1.py
+++ Lab1/hw1.py
@@ -8,7 +8,9 @@ def read_input(filename):
 		read = csv.reader(file, delimiter = ',')
 		for row in read:
 			x.append(float(row[0]))
-			y.append(float(row[1]))
+			temp = []
+			temp.append(float(row[1]))
+			y.append(temp) #change y into 2D list
 	return x, y
 
 def matrix_A(x, polynomial_bases):
@@ -29,7 +31,39 @@ def matrix_transpose(x):
 		transpose.append(temp)
 	return transpose
 
-#def matrix_mul():
+def matrix_mul(A, B):
+	result = []
+	for i in range(len(A)):
+		temp = []
+		for j in range(len(B[0])):
+			temp.append(0)
+		result.append(temp)
+	for i in range(len(A)):
+		for j in range(len(B[0])):
+			for k in range(len(B)):
+				result[i][j] += A[i][k] * B[k][j]
+	return result
+
+def I_mul_scalar(scalar, size):
+	result = []
+	for i in range(size):
+		temp = []
+		for j in range(size):
+			if(i == j):
+				temp.append(scalar)
+			else:
+				temp.append(0)
+		result.append(temp)
+	return result
+
+def matrix_add(A, B):
+	result = []
+	for i in range(len(A)):
+		temp = []
+		for j in range(len(A[0])):
+			temp.append(A[i][j] + B[i][j])
+		result.append(temp)
+	return result
 
 if __name__ == "__main__":
 	filename = sys.argv[1]
@@ -38,3 +72,7 @@ if __name__ == "__main__":
 	x, y = read_input(filename)
 	A = matrix_A(x, polynomial_bases)
 	A_transpose = matrix_transpose(A)
+	A_transpose_mul_A = matrix_mul(A_transpose, A)
+	lambda_I = I_mul_scalar(lse_lambda, len(A_transpose_mul_A))
+	A_transpose_mul_A_add_lambda_I = matrix_add(A_transpose_mul_A, lambda_I)
+	A_transpose_mul_b = matrix_mul(A_transpose, y)
