caf5a8a0038be1aa76b27405b5fcd6d875e3e9bd
leeang6969 <leeang6969.cs05@g2.nctu.edu.tw>
2019-05-29 18:25:05 +0800

    Update k-means.py

--- Lab6/k-means.py
+++ Lab6/k-means.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
+from matplotlib.axes._axes import _log as matplotlib_axes_logger
+matplotlib_axes_logger.setLevel('ERROR')
 
 def read_input():
 	with open('moon.txt') as csv_file:
@@ -16,7 +18,7 @@ def read_input():
 def initialization(k):
 	means = np.random.rand(k, 2)
 	previous_classification = np.zeros([3000], np.int)
-	return means, previous_classification
+	return means, previous_classification, 1 # 1 for iteration
 
 def classify(data, means):
 	classification = np.zeros([3000], dtype=int)
@@ -41,29 +43,35 @@ def update(data, means, classification):
 		count[classification[i]] += 1
 	return np.true_divide(means, count)
 
-def draw(data, means, classification):
-	plt.title("K-Means")
-	colors = ['g', 'b']
+def draw(data, means, classification, iteration, error):
+	color = iter(plt.cm.rainbow(np.linspace(0, 1, means.shape[0] * 2)))
+	title = "K-Means Iteration-" + str(iteration)
+	plt.title(title)
+	plt.clf()
 	for i in range(0, means.shape[0]):
+		col = next(color)
 		for j in range(0, data.shape[0]):
 			if classification[j] == i:
-				plt.scatter(data[j][0], data[j][1], s=8, c=colors[i])
-	plt.scatter(means[:,0], means[:,1], s=32, c='r')
-	plt.show()
+				plt.scatter(data[j][0], data[j][1], s=8, c=col)
+	for i in range(0, means.shape[0]):
+		col = next(color)
+		plt.scatter(means[i][0], means[i][1], s=32, c=col)
+	plt.savefig(title+'.png')
 
 def k_means(data):
 	# k is the number of cluster
 	k = 2
-	means, previous_classification = initialization(k) # temp size: k*2 previous_classification: 3000
+	means, previous_classification, iteration = initialization(k) # means size: k*2 previous_classification: 3000
 	classification = classify(data, means) # classification: 3000
 	error = calculate_error(classification, previous_classification)
-	draw(data, means, classification)
+	draw(data, means, classification, iteration, error)
 	while(error):
+		iteration += 1
 		means = update(data, means, classification)
 		previous_classification = classification
 		classification = classify(data, means)
 		error = calculate_error(classification, previous_classification)
-		draw(data, means, classification)
+		draw(data, means, classification, iteration, error)
 		print(error)
 
 if __name__ == "__main__":
